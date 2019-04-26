5249b6562c6afe38efe6c21c1c58bd23b58703f2
Yuri Podolsky <podolsky4@gmail.com>
2019-04-26 19:57:14 +0300

    Add initial Spring Boot config (#3)
    * Add Spring Boot config
    
    * Change CircleCI docker image JDK version

---
+++ src/test/java/com/mangalaxy/mango/MangoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mangalaxy.mango;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MangoApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
