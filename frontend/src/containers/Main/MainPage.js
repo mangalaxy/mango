13bed767b31c1517c61286eaa266dbbfa1b921a0
Yuri Podolsky <podolsky4@gmail.com>
2019-11-10 16:38:39 +0200

    Update MainPage.js
    Change route for About page

--- frontend/src/containers/Main/MainPage.js
+++ frontend/src/containers/Main/MainPage.js
@@ -29,7 +29,7 @@ class MainPage extends Component<Props> {
               <Route path={routes.FOR_EMPLOYERS} component={ForEmployers}/>
               <Route path={routes.FIND_JOB} component={JobsPage}/>
               <Route path={routes.BLOG} component={BlogPage}/>
-              <Route path={routes.ABOUT_AS} component={About}/>
+              <Route path={routes.ABOUT} component={About}/>
               <Route path={'/*'} component={()=>(<div>404</div>)}/>
             </Switch>
           </div>
