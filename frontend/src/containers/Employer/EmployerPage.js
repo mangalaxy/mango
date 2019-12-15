f266f88bc7e0de017a07bc822a26e44d727ff9bb
Yevhenii Krechetov <49247154+ekrechetov@users.noreply.github.com>
2019-12-15 19:29:55 +0200

    Edited EmployerPage, added EmployerWelcome (#43)
    * Edited EmployerPage
    
    * Added employer-welcome component
    
    * Edited Welcome component
    
    * Edited Welcome component

--- frontend/src/containers/Employer/EmployerPage.js
+++ frontend/src/containers/Employer/EmployerPage.js
@@ -1,5 +1,7 @@
 import React, {Component, Fragment} from 'react';
 import EmployerMenu from '../../components/Employer/EmployerMenu/EmployerMenu';
+import EmployerWelcome from '../../components/Employer/EmployerWelcome/EmployerWelcome';
+import './EmployerPage.scss';
 
 class Employer extends Component {
   render(): React.ReactElement<any> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
@@ -7,7 +9,7 @@ class Employer extends Component {
         <Fragment>
           <EmployerMenu currentPage={this.props.history.location.pathname}/>
           <div className="employersContent">
-            
+            <EmployerWelcome />
           </div>
         </Fragment>
     );
