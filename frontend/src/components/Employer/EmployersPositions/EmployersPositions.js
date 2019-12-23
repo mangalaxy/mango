2cc05d898db3cbd2266fe9b0662282033e840cc1
Yevhenii Krechetov <49247154+ekrechetov@users.noreply.github.com>
2019-12-23 11:09:26 +0200

    Employers-positions (#45)
    * Edited EmployerPage
    
    * Added employer-welcome component
    
    * Edited Welcome component
    
    * Edited Welcome component
    
    * Created EmployerPosition Component
    
    * Edited employers-positions page whith add Switch component
    
    * Edited employers-posiyions component
    
    * Added reducer, action for employers-positions
    
    Co-authored-by: Yuri Podolsky <podolsky4@gmail.com>

---
+++ frontend/src/components/Employer/EmployersPositions/EmployersPositions.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import EmployersPositionItem from './EmployersPositionItem';
+import './EmployersPositions.scss';
+   
+class EmployersPositions extends Component {
+  render() {
+    return (
+      <div className="employersPositions">
+
+        <Link to="#" className="employersPositions-btn">
+          + create new position
+        </Link>
+
+        <h3 className="employersPositions-title">
+          Your positions
+        </h3>
+
+        <div className="employersPositions-content">
+          {this.props.jobs.map(item => (
+            <EmployersPositionItem key={item.id} itemData={item} />
+          ))}
+        </div>
+
+      </div>
+    )
+  }
+}
+const mapStoreToProps = (store) => {
+  return {jobs: store.jobReducer}
+}
+export default connect(mapStoreToProps)(EmployersPositions);
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
 import user from './user';
+import jobReducer from './jobReducer';
 import {reducer as formReducer} from 'redux-form';
 
 export default combineReducers({
   routing: routerReducer,
   user,
+  jobReducer,
   form: formReducer
 });
