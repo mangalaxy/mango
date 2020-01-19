bb7ed5b214e23f33d8860bccc65d7cd86c660324
Krechetov Yevhenii <jenya-99@ukr.net>
2020-01-20 00:30:49 +0200

    Edited bookmarked-talents

--- frontend/src/components/Employer/BookmarkedTalents/BookmarkedTalents.js
+++ frontend/src/components/Employer/BookmarkedTalents/BookmarkedTalents.js
@@ -4,20 +4,28 @@ import iconLeft from '../../../assets/icons/left.svg';
 import iconRight from '../../../assets/icons/right.svg';
 import MatchedTalentsItem from '../MatchedTalents/MatchedTalentsItem';
 import './BookmarkedTalents.scss';
+
 function BookmarkedTalents(props) {
+  let markedTalents = props.talents.filter(item => {
+    return item.bookmarked === true    
+  });
   return(
-    <div>  
+    <div>
       <div className="bookmarked-container">
         {props.talents.map(item => (
-            !item.bookmarked ? null :
+            !item.bookmarked ?
+            null:            
             <MatchedTalentsItem key={item.id} talentData={item} />
           ))            
         }
+        {markedTalents.length != 0 ?
         <p className="running-title">
           <img src={iconLeft} alt="Left icon" />
             1 2 3 ... 19 20
           <img src={iconRight} alt="Right icon" /> 
-        </p>
+        </p> :
+        <span className="bookmarked-empty">no bookmarked talents</span>
+        }   
       </div>
     </div>  
   )
