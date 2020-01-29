c286ad8b362bae13fe3ebaf558d0af21ff91d2a5
kopinalex <kopin.alex@gmail.com>
2020-01-29 19:06:44 +0200

    company page

--- frontend/src/components/Employer/Company/Company.js
+++ frontend/src/components/Employer/Company/Company.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React from 'react';
+import './company.scss';
+import {GreenDash} from '../../Main/Home/Home';
+import SvgIcon from '../../SvgIcon/SvgIcon';
+import EditIcon from '../../elements/icons/EditIcon';
 
 const Company = ({companyProfile}) => (
-    <>
-      <div>
-        <img src={companyProfile.logo} alt=""/>
+    <div className='companyPage'>
+      <div className="contentContainer">
+        <div className='companyHeader'>
+          <div className="content">
+            <div className='logoContainer'>
+              <img src={companyProfile.logo} alt=""/>
+            </div>
+
+            <div>
+              <h4>Company</h4>
+              <h2>{companyProfile.name}</h2>
+              <GreenDash/>
+            </div>
+          </div>
+          <div>
+            <EditIcon/>
+          </div>
+        </div>
       </div>
+
       <div>
 
       </div>
-    </>
+    </div>
 );
 
 export default Company;
