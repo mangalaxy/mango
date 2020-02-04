a27cdbf303225d285c4db37f653d864e66ec7717
Yevhenii Krechetov <49247154+ekrechetov@users.noreply.github.com>
2020-02-04 13:29:20 +0200

    Edited bookmarked-talents page (#53)
    * Created Matched-talents page
    
    * edited MatchedTalents page
    
    * Added talents item to matched-talents page
    
    * Edited MatchedTalentsItem component
    
    * Added bookmarked talent state to store
    
    * Adde d delete talents with popup modal
    
    * Added remove position and remove talent with modal
    
    * Added bookmarked-talents page
    
    * Edited bookmarked-talents
    
    * Updating branch
    
    * Сorrections after comments
    
    Co-authored-by: Yuri Podolsky <podolsky4@gmail.com>

--- frontend/src/components/Employer/EmployersPositions/EmployersPositionItem.js
+++ frontend/src/components/Employer/EmployersPositions/EmployersPositionItem.js
@@ -1,32 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {connect} from 'react-redux';
+import {jobDeleteAction} from '../../../actions/jobDeleteAction';
 import Switch from '../../Switch/Switch';
-import icon from '../../../assets/icons/bag.svg';
+import iconBag from '../../../assets/icons/bag.svg';
+import iconCross from '../../../assets/icons/cross.svg';
+import Modal from '../../Modal/Modal';
 
 const EmployersPositionItem = (props) => {
-  let {id, position, city, country, isChecked, date} = props.itemData;
+  const [openModal, setOpenModal] = useState(false);
+  let {id, position, location: {city, country}, isChecked, date} = props.itemData;
+
+  function linkTo(e) {
+    if (e.target.tagName === 'SPAN' ||
+        e.target.tagName === 'INPUT' ||
+        e.target.tagName === 'IMG')
+      {
+      return;
+    } else {
+      props.history.push(`/employers/matched-talents/${id}`);
+    }
+  }
+
+  function toggleModal() {
+    setOpenModal(!openModal);
+  }
+  function removeJob() {
+    props.dispatch(jobDeleteAction(+id));
+  }
   return(
-    <div className="employersPositions-item">
-
-      <div className="employersPositions-item-icon">
-        <div className="employersPositions-item-icon-box">
-          <img src={icon} width="25px" height="25px"/>
+    <>
+      <div onClick={linkTo} className="employersPositions-item">
+        <div className="employersPositions-item-icon">
+          <div className="employersPositions-item-icon-box">
+            <img src={iconBag} width="25px" height="25px" alt="bag icon"/>
+          </div>
         </div>
-      </div>
+        <div className="employersPositions-item-description">
+          <p>
+            {position} <br></br>
+            {city},<br></br>
+            {country}          
+          </p>
+        </div>
+        <div className="employersPositions-item-control">
+          <Switch isChecked={isChecked} name={id}/>
 
-      <div className="employersPositions-item-description">
-        <p>
-          {position} <br></br>
-          {city},<br></br>
-          {country}          
-        </p>
-      </div>
+          <span onClick={toggleModal} className="employersPositions-item-control-close">
+            <img src={iconCross} alt="cross icon" />
+          </span>
 
-      <div className="employersPositions-item-swith">
-        <Switch isChecked={isChecked} name={id}/>
-        <div>{date}</div>
+          <div>{date}</div>
+        </div>      
       </div>
-
-    </div>
+      {!openModal ? 
+        null :
+        <Modal modalText = {'Are you sure you want to remove this position?'}
+             canсelModal = {toggleModal}
+             modalAction = {removeJob}
+        />
+      }      
+    </>   
   )
 }
-export default EmployersPositionItem;
+export default connect()(EmployersPositionItem);
