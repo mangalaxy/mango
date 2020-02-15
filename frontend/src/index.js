28941c6e546b99e65dada4755dbcbbc0dd9f1bee
DedNikolai <38811381+DedNikolai@users.noreply.github.com>
2020-02-15 22:26:22 +0200

    add-interview-page-for-talent (#51)
    * add-interview-page-for-talent
    
    * Add Git ignoring compiled css files
    
    Signed-off-by: Yuri Podolsky <podolsky4@gmail.com>
    
    * Clean up code
    
    Signed-off-by: Yuri Podolsky <podolsky4@gmail.com>
    
    * Slightly refactor frontend code
    
    Signed-off-by: Yuri Podolsky <podolsky4@gmail.com>
    
    Co-authored-by: Yuri Podolsky <podolsky4@gmail.com>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import {render} from 'react-dom'
-import App from './App'
+import ReactDOM from 'react-dom'
+import {Provider} from "react-redux";
+import store from './store';
+import App from './App';
 
-render(
-    <App/>,
-    document.getElementById('root')
+ReactDOM.render(
+      <Provider store={store}>
+        <App/>
+      </Provider>,
+      document.getElementById('root')
 );
