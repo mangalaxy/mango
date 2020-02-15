1459e455a2317eca0775fac26e6c942301523bce
Yuri Podolsky <podolsky4@gmail.com>
2020-02-15 23:50:19 +0200

    Fixed linting and some warnings
    Signed-off-by: Yuri Podolsky <podolsky4@gmail.com>

--- frontend/src/components/Forms/LoginForm/index.js
+++ frontend/src/components/Forms/LoginForm/index.js
@@ -1,44 +1,48 @@
-import {Formik} from 'formik';
 import React from 'react';
-import LoginFormView from './LoginFormView';
+import {Formik} from 'formik';
 import * as Yup from 'yup';
+import LoginFormView from './LoginFormView';
 
-const contactSchema = Yup.object().shape({
-  name: Yup.string().
-      min(2, 'Minimum 2 symbols').
-      max(30, 'Maximum 30 symbols').
-      required('Required field'),
-  email: Yup.string().email('Invalid email format').required('Required field'),
-  message: Yup.string().required('Required field'),
-});
+const contactSchema = Yup.object().shape(
+      {
+        name: Yup.string()
+              .min(2, 'Minimum 2 symbols')
+              .max(30, 'Maximum 30 symbols')
+              .required('Required field'),
+        email: Yup.string()
+              .email('Invalid email format')
+              .required('Required field'),
+        message: Yup.string()
+              .required('Required field'),
+      }
+);
 
 const LoginForm = ({onSuccess, onError}) => (
-    <Formik
-        onSubmit={(
+      <Formik onSubmit={(
             {email, password, rememberMe}, {resetForm, setStatus, setSubmitting}) => {
-          setStatus({});
-          try {
-            let data = {};
-            data.email = email;
-            data.password = password;
-            data.rememberMe = rememberMe;
+        setStatus({});
+        try {
+          let data = {};
+          data.email = email;
+          data.password = password;
+          data.rememberMe = rememberMe;
 
-            // TODO: do query to API
-            onSuccess();
-            // resetForm();
-          } catch (err) {
-            setStatus({failed: true});
-            setSubmitting(false);
-            onError();
-          }
-
-        }
+          // TODO: do query to API
+          onSuccess();
+          // resetForm();
+        } catch (err) {
+          setStatus({failed: true});
+          setSubmitting(false);
+          onError();
         }
 
-        component={LoginFormView}
-        validationSchema={contactSchema}
-        initialValues={{}}
-    />
+      }
+      }
+
+              component={LoginFormView}
+              validationSchema={contactSchema}
+              initialValues={{}}
+      />
 );
 
 export default LoginForm;
