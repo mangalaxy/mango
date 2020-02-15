9d0bcc5b4ef92a343dffc1c4c3878669cfd6a071
Yuri Podolsky <podolsky4@gmail.com>
2020-02-15 22:16:32 +0200

    Slightly refactor frontend code
    Signed-off-by: Yuri Podolsky <podolsky4@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,34 +5,20 @@ import './styles/index.scss';
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
-import {Provider} from 'react-redux';
-import {applyMiddleware, createStore} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import reducer from './reducers';
-import MainPage from './containers/Main/MainPage';
 import {routes} from './constants/routes';
+import MainPage from './containers/Main/MainPage';
 import Talent from './containers/Talent/TalentHome';
 import Employer from './containers/Employer/EmployerPage';
 import CreateProfile from "./components/Profile/CreateProfile/CreateProfile";
 import ProfileInterview from './components/ProfileInterview/ProfileInterview'
 import TalentProfile from "./containers/TalentProfile/TalentProfile";
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
-
 type Props = {};
 type State = {};
 
 class App extends React.Component <Props, State> {
   props: Props;
-  state: State = {};
-
-  componentDidMount() {
-  }
-
-  componentWillUnmount() {
-  }
 
   render() {
     const ScrollToTop = () => {
@@ -40,7 +26,6 @@ class App extends React.Component <Props, State> {
       return null;
     };
     return (
-        <Provider store={store}>
           <Router>
             <Route component={ScrollToTop} />
             <Switch>
@@ -52,7 +37,6 @@ class App extends React.Component <Props, State> {
               <Route path={routes.HOME} component={MainPage}/>
             </Switch>
           </Router>
-        </Provider>
     );
   }
 }
--- frontend/src/components/Main/MainMenu/MainMenu.js
+++ frontend/src/components/Main/MainMenu/MainMenu.js
@@ -2,14 +2,16 @@
 import type {Node} from 'react';
 import React, {PureComponent} from 'react';
 import {Link, NavLink} from 'react-router-dom';
-import {routes} from '../../../constants/routes';
+import Down from "../../Down/Down";
 
+import {routes} from '../../../constants/routes';
 import './MainMenu.scss';
 
 type Props = {
   path: string,
   openLoginForm: ()=>void
 };
+
 type State = {
   dropdownVisible: boolean
 }
@@ -23,61 +25,60 @@ export default class MainMenu extends PureComponent<Props, State> {
   showMenu = () => {
     this.setState({dropdownVisible: true});
   };
+
   hideMenu = () => {
     this.setState({dropdownVisible: false});
   };
 
   render(): Node {
-
-    const {path, grey, dark, openLoginForm, openSignUpTalent, openSignUpEmployer} = this.props;
+    const { path, grey, dark, openLoginForm, openSignUpTalent, openSignUpEmployer } = this.props;
     let grayMode = !(path === routes.HOME || path === routes.FOR_TALENTS ||
-        path === routes.FOR_EMPLOYERS || path === routes.ABOUT_AS || path ===
-        routes.BLOG);
+          path === routes.FOR_EMPLOYERS || path === routes.ABOUT_AS || path ===
+          routes.BLOG);
 
-    const {dropdownVisible} = this.state;
+    const { dropdownVisible } = this.state;
     return (
-        <div className={`menuContainer ${grayMode || grey && 'gray'}`}>
-          <div className='logoContainer'>
-            <Link to={routes.HOME} className='logo'>
-              Mango
-            </Link>
-          </div>
-          <ul className='menuItemsContainer'>
-            <li>
-              <NavLink to={routes.FOR_TALENTS}
-                       activeClassName={'menuItem active'}
-                       className={`menuItem ${grayMode || grey && 'gray'}`}>
-                Talents
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to={routes.FOR_EMPLOYERS}
-                       activeClassName={'menuItem active'}
-                       className={`menuItem ${grayMode || grey && 'gray'}`}>
-                Employers
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to={routes.FIND_JOB}
-                       activeClassName={'menuItem active'}
-                       className={`menuItem ${grayMode || grey && 'gray'}`}>
-                Jobs
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to={routes.BLOG}
-                       activeClassName={'menuItem active'}
-                       className={`menuItem ${grayMode || grey && 'gray'}`}>
-                Blog
-              </NavLink>
-            </li>
-          </ul>
-          <div className='menuItemsContainer authLinks'>
-            <div onClick={openLoginForm}>
-              <span
-                  className={`menuItem  ${grayMode || grey && 'gray'} authBlock left`}>Log in</span>
+          <div className={`menuContainer ${grayMode || grey && 'gray'}`}>
+            <div className='logoContainer'>
+              <Link to={routes.HOME} className='logo'>
+                Mango
+              </Link>
             </div>
-            <div>
+            <ul className='menuItemsContainer'>
+              <li>
+                <NavLink to={routes.FOR_TALENTS}
+                         activeClassName={'menuItem active'}
+                         className={`menuItem ${grayMode || grey && 'gray'}`}>
+                  Talents
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to={routes.FOR_EMPLOYERS}
+                         activeClassName={'menuItem active'}
+                         className={`menuItem ${grayMode || grey && 'gray'}`}>
+                  Employers
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to={routes.FIND_JOB}
+                         activeClassName={'menuItem active'}
+                         className={`menuItem ${grayMode || grey && 'gray'}`}>
+                  Jobs
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to={routes.BLOG}
+                         activeClassName={'menuItem active'}
+                         className={`menuItem ${grayMode || grey && 'gray'}`}>
+                  Blog
+                </NavLink>
+              </li>
+            </ul>
+            <div className='menuItemsContainer authLinks'>
+              <div onClick={openLoginForm}>
+                <span className={`menuItem  ${grayMode || grey && 'gray'} authBlock left`}>Log in</span>
+              </div>
+              <div>
               <span className={`menuItem  ${grayMode || grey && 'gray'} authBlock`}
                     onClick={this.showMenu}>
                 Sign up
@@ -90,22 +91,9 @@ export default class MainMenu extends PureComponent<Props, State> {
                     <li onClick={openSignUpEmployer}>Employers</li>
                 </ul>
               </span>
+              </div>
             </div>
-
           </div>
-        </div>
     );
   }
 }
-
-const Down = (props) => {
-  const {active, grayMode} = props;
-  return (<svg
-      className={`down ${active && 'active'} ${grayMode && 'gray'}`}
-      width="17" height="10" viewBox="0 0 17 10"
-      fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-        d="M16.8301 1.03084L15.9785 0.171877C15.865 0.057172 15.7343 0 15.5865 0C15.4391 0 15.3084 0.057172 15.1949 0.171877L8.50009 6.92438L1.80555 0.172058C1.69201 0.0573527 1.56135 0.000180444 1.41374 0.000180444C1.26607 0.000180444 1.13541 0.0573527 1.02192 0.172058L0.1704 1.03108C0.0566808 1.14555 0 1.27734 0 1.42629C0 1.57512 0.0568598 1.70692 0.1704 1.82138L8.10828 9.82824C8.22176 9.94277 8.35248 10 8.50009 10C8.6477 10 8.77818 9.94277 8.8916 9.82824L16.8301 1.82138C16.9436 1.70686 17 1.57506 17 1.42629C17 1.27734 16.9436 1.14555 16.8301 1.03084Z"
-        fill="#F2F2F2"/>
-  </svg>);
-};
