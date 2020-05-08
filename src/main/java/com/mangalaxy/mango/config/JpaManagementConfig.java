57069ef24c8d1cd1c22d2d2aaf18b6be747cda9b
Yuri Podolsky <podolsky4@gmail.com>
2020-05-08 17:57:31 +0300

    Update dependencies version and add model mapper configuration
    Signed-off-by: Yuri Podolsky <podolsky4@gmail.com>

--- src/main/java/com/mangalaxy/mango/config/JpaManagementConfig.java
+++ src/main/java/com/mangalaxy/mango/config/JpaManagementConfig.java
@@ -1,7 +1,5 @@
 package com.mangalaxy.mango.config;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -9,11 +7,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableTransactionManagement
 @EnableJpaAuditing(modifyOnCreate = false)
-public class ApplicationConfig {
-
-  @Bean(name = "dtoMapper")
-  public ModelMapper modelMapper() {
-    return new ModelMapper();
-  }
+public class JpaManagementConfig {
 
 }
