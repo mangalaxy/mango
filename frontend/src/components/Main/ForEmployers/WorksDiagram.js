e2fe9d71eb9ebf44628bad1a0f007b0f11a3bf0e
kopinalex <kopin.alex@gmail.com>
2020-08-16 20:24:59 +0300

    for talents. for employers adaptive

--- frontend/src/components/Main/ForEmployers/WorksDiagram.js
+++ frontend/src/components/Main/ForEmployers/WorksDiagram.js
@@ -1,6 +1,6 @@
-import React, {Fragment} from "react";
+import React, {Fragment} from 'react';
 
-import "./ForEmloyers.scss";
+import './ForEmloyers.scss';
 
 // TODO: некорретно отображается в Firefox, возможно, переделать принципиально по-другому
 //  не работает calc, возможно все завязать на %, включая круги
@@ -13,18 +13,20 @@ const WorksDiagram = () => {
     {text: 'Have an interview', x: 78, y: 90},
     {text: 'Close a candidate', x: 95, y: 10},
   ];
-  return (<div className='worksDiagramContainer'>
-    {data.map((item, index) => (
-          <Fragment key={index}>
-            <div className="outerCircle"
-                 style={{top: `${item.y}%`, left: `${item.x}%`}}>
-              <div className="innerCircle">
+  return (
+      <>
+        <div className='worksDiagramContainer wide'>
+          {data.map((item, index) => (
+              <Fragment key={index}>
+                <div className="outerCircle"
+                     style={{top: `${item.y}%`, left: `${item.x}%`}}>
+                  <div className="innerCircle">
             <span className='order'>
               {index + 1}
               <span className='pointHeader'
                     style={{
                       transform: `translateX(-50%) translateY(${
-                            index % 2 ? 'calc(-50% - 40px)' : 'calc(50% + 40px)'
+                          index % 2 ? 'calc(-50% - 40px)' : 'calc(50% + 40px)'
                       })`,
                     }}
 
@@ -32,21 +34,44 @@ const WorksDiagram = () => {
                 {item.text}
               </span>
           </span>
-              </div>
-            </div>
-            {index < data.length - 1 &&
-            <svg height="100%" width="100%"
-                 style={{position: 'absolute', left: `0`, top: `0`}}>
-              <line x1={`calc(${item.x}% + 52px)`}
-                    y1={`calc(${item.y}% ${index % 2 ? '+' : '-'} 11%)`}
-                    x2={`${data[index + 1].x}%`} y2={`${index % 2 ?
-                    data[index + 1].y - 11 :
-                    data[index + 1].y + 11}%`}
-                    stroke='rgba(54, 179, 168, 0.9)' strokeWidth='2'/>
-            </svg>}
-          </Fragment>
-    ))}
-  </div>);
+                  </div>
+                </div>
+                {index < data.length - 1 &&
+                <svg height="100%" width="100%"
+                     style={{position: 'absolute', left: `0`, top: `0`}}>
+                  <line x1={`calc(${item.x}% + 52px)`}
+                        y1={`calc(${item.y}% ${index % 2 ? '+' : '-'} 11%)`}
+                        x2={`${data[index + 1].x}%`} y2={`${index % 2 ?
+                      data[index + 1].y - 11 :
+                      data[index + 1].y + 11}%`}
+                        stroke='rgba(54, 179, 168, 0.9)' strokeWidth='2'/>
+                </svg>}
+              </Fragment>
+          ))}
+        </div>
+
+        <div className='worksDiagramMobile'>
+          {data.map((step, index) => {
+            return (
+                <div className='worksDiagramMobile__item' key={index}>
+                  <div className={`worksDiagramMobile__item-container`}>
+                    <div className={`worksDiagramMobile__text-block`}>
+                      <div className='worksDiagramMobile__cycle'>
+                        <div className='worksDiagramMobile__inner-cycle'>
+                          {index + 1}
+                        </div>
+                      </div>
+                      <div className='worksDiagramMobile__line'>
+                        <div className={`worksDiagramMobile__step-header`}>{step.text}</div>
+                      </div>
+                      <div className='worksDiagramMobile__small-cycle'/>
+                    </div>
+                  </div>
+                </div>
+            );
+          })}
+        </div>
+      </>);
 };
 
 export default WorksDiagram;
--- frontend/src/components/Main/ForTalents/ForTalents.js
+++ frontend/src/components/Main/ForTalents/ForTalents.js
@@ -1,57 +1,67 @@
-import React, {Fragment} from "react";
-import {GreenDash, Partners} from "../Home/Home";
+import React, {Fragment} from 'react';
+import {Partners} from '../Home/Home';
 
-import "./ForTalents.scss";
+import './ForTalents.scss';
 
-const ForTalents = () =>{
-    return (
-        <Fragment>
-          <div className="bannerContainer" id='for-talent-banner'>
-            <div className="image-filter"/>
-            <div className="bannerContent md-6of12">
-              <h1 className="bannerContent-title left">Our advantages</h1>
-              <ul>
-                <li className="bannerContent-description">The best companies in the world are looking for you.</li>
-                <li className="bannerContent-description">Speedy job search.</li>
-                <li className="bannerContent-description"> The whole process of hiring is under control (feedback,
-                  interview results, time management).
-                </li>
-                <li className="bannerContent-description">Job search process is fully under your control.</li>
-              </ul>
-            </div>
+const ForTalents = () => {
+  return (
+      <Fragment>
+        <div className="bannerContainer" id='for-talent-banner'>
+          <div className="image-filter"/>
+          <div className="bannerContent md-6of12">
+            <h1 className="bannerContent-title left">Our advantages</h1>
+            <ul>
+              <li className="bannerContent-description">The best companies in
+                the world are looking for you.
+              </li>
+              <li className="bannerContent-description">Speedy job search.</li>
+              <li className="bannerContent-description"> The whole process of
+                hiring is under control (feedback,
+                interview results, time management).
+              </li>
+              <li className="bannerContent-description">Job search process is
+                fully under your control.
+              </li>
+            </ul>
           </div>
+        </div>
 
-          <section className="our-help">
+        <div className="container">
+          <div className="our-help">
             <div className="contentContainer md-6of12">
-              <h3 className="mainHeaderH3">Support from our career advocates</h3>
-              <GreenDash/>
+              <h2 className="mainHeaderH2">Support from our career
+                advocates</h2>
               <ul>
                 <li>Professional advice on creating a strong impression on
-                  potential employers.</li>
+                  potential employers.
+                </li>
                 <li>Objective assessment of companies and selection the most
-                  suitable for you.</li>
+                  suitable for you.
+                </li>
                 <li>Support and follow-up of your interviews throughout the
-                  negotiation process, until getting a job offer.</li>
+                  negotiation process, until getting a job offer.
+                </li>
                 <li>Expert assessment of global trends in the development of
-                  projects and tendencies in wage growth.</li>
+                  projects and tendencies in wage growth.
+                </li>
               </ul>
             </div>
-            <div className="imageContainer md-6of12">
-              <img src="/images/advocate.png" alt="Career advocate" className="responsive"/>
+            <div className="imageContainer">
+              <img src="/images/advocate.png" alt="Career advocate"/>
             </div>
-          </section>
+          </div>
+        </div>
 
-          <section className="goal">
-            <div className="imageContainer md-6of12">
-              <img src="/images/goal.png" alt="Goal" className="responsive"/>
-            </div>
-            <div className="contentContainer md-6of12">
-              <h4>What are you waiting for?</h4>
-              <button className="accent-btn">Apply now</button>
-            </div>
-          </section>
+        <section className="goal">
+          <div className="halfBanner"/>
+          <div className="contentContainer md-6of12">
+            <h4>What are you waiting for?</h4>
+            <button className="accent-btn">Apply now</button>
+          </div>
+        </section>
 
-          <section className="how-it-works">
+        <div className="container">
+          <div className="how-it-works wide">
             <div className="contentContainer md-6of12">
               <h2 className="mainHeaderH2">How it works</h2>
               <p>Show your skills and aspirations to work for influential
@@ -59,30 +69,57 @@ const ForTalents = () =>{
               <p>Accept interview requests from leading companies and your
                 chances of getting a desired job skyrocket.</p>
             </div>
-            <div className="imageContainer md-6of12">
-              <img src="/images/how_it_works.png" alt="How it works" className="responsive"/>
+            <div className="imageContainer comp">
+              <img src="/images/how_it_works.png" alt="How it works"
+                   className="responsive"/>
             </div>
-          </section>
+          </div>
+        </div>
 
-          <section className="why-trusted">
-            <h2 className="mainHeaderH2">Why we are trusted</h2>
-            <ul>
-              <li>Platform that allows you to avoid intermediaries between you
-                and potential employers.</li>
-              <li>Individual approach to every talent.</li>
-              <li>Free registration and easy access to negotiation with employers.</li>
-              <li>Involvement with most suitable projects and companies based on
-                your tech stack and experience.</li>
-              <li>Vast selection of open positions from leading companies.</li>
-            </ul>
-          </section>
+        <div className="container">
+          <div className="why-trusted">
+            <div className="imageContainer wide">
+              <img src="/images/why_trusted.png" alt=""/>
+            </div>
+            <div className="imageContainer mobile">
+              <div className="contentContainer">
+                <h2 className="mainHeaderH2">How it works</h2>
+                <p>Show your skills and aspirations to work for influential
+                  companies, and we will help set up your profile.</p>
+                <p>Accept interview requests from leading companies and your
+                  chances of getting a desired job skyrocket.</p>
+              </div>
+              <img src="/images/Iphone.png" alt=""/>
+            </div>
 
-          <section id="global">
-            <h2 className='mainHeaderH2'>Global business</h2>
-            <Partners/>
-          </section>
-        </Fragment>
-    );
-}
+            <div className='contentContainer'>
+              <h2 className="mainHeaderH2">Why we are trusted</h2>
+              <ul>
+                <li>Platform that allows you to avoid intermediaries between you
+                  and potential employers.
+                </li>
+                <li>Individual approach to every talent.</li>
+                <li>Free registration and easy access to negotiation with
+                  employers.
+                </li>
+                <li>Involvement with most suitable projects and companies based
+                  on
+                  your tech stack and experience.
+                </li>
+                <li>Vast selection of open positions from leading companies.
+                </li>
+              </ul>
+            </div>
+
+          </div>
+        </div>
+
+        <div id="global" className='container'>
+          <h2 className='mainHeaderH2'>Global business</h2>
+          <Partners/>
+        </div>
+      </Fragment>
+  );
+};
 
 export default ForTalents;
