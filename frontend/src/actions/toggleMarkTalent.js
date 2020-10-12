b6c8d56ba8ac67e6e7cd54af80b6f97a0d38566f
Yuri Podolsky <podolsky4@gmail.com>
2020-10-12 20:55:47 +0300

    Migrate frontend code to dedicated repository
    Signed-off-by: Yuri Podolsky <podolsky4@gmail.com>

--- frontend/src/actions/toggleMarkTalent.js
+++
@@ -1,8 +0,0 @@
-import {TOGGLE_MARK_TALENT} from "./action_types";
-
-export const toggleMarkTalent = (id) => {
-  return {
-    type: TOGGLE_MARK_TALENT,
-    payload: id
-  }
-}
--- frontend/src/constants/colors.js
+++
@@ -1,7 +0,0 @@
-export default {
-    COLOR_GREY: '#808080',
-    COLOR_PRIMERY: '#333333'
-}
-
-
-
