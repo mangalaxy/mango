29be3918329075b6908e9bb4cda3fc3719c7741e
Yuri Podolsky <podolsky4@gmail.com>
2020-11-13 15:45:40 +0200

    Expand SwaggerConfig and add Jackson datatype dependency
    Signed-off-by: Yuri Podolsky <podolsky4@gmail.com>

--- src/main/java/com/mangalaxy/mango/config/SwaggerConfig.java
+++ src/main/java/com/mangalaxy/mango/config/SwaggerConfig.java
@@ -2,22 +2,46 @@ package com.mangalaxy.mango.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import static springfox.documentation.builders.PathSelectors.ant;
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport {
 
   @Bean
-  public Docket api() {
+  public Docket mangoApi() {
     return new Docket(DocumentationType.SWAGGER_2)
           .select()
-          .apis(RequestHandlerSelectors.any())
-          .paths(PathSelectors.any())
+          .apis(basePackage("com.mangalaxy.mango.controller"))
+          .paths(ant("/api/**"))
+          .build()
+          .apiInfo(metaData());
+  }
+
+  private static ApiInfo metaData() {
+    return new ApiInfoBuilder()
+          .title("MANGOSTART REST API")
+          .description("REST API for ManGoStart recruiting platform")
+          .version("1.0.0-SNAPSHOT")
+          .license("MIT License")
+          .licenseUrl("https://opensource.org/licenses/MIT")
           .build();
+  }
+
+  @Override
+  protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("swagger-ui.html")
+          .addResourceLocations("classpath:/META-INF/resources/");
+    registry.addResourceHandler("/webjars/**")
+          .addResourceLocations("classpath:/META-INF/resources/webjars/");
   }
 }
