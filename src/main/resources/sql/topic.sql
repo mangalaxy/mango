d9960071be28006b7aa3c1dfb6291d3ac7ac13b4
Yuri Podolsky <podolsky4@gmail.com>
2020-12-17 16:44:53 +0200

    Add sql files to fill up (#108)
    Signed-off-by: Yuri Podolsky <podolsky4@gmail.com>

---
+++ src/main/resources/sql/topic.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.topic (title)
+VALUES ('Interview process'),
+       ('Engineering'),
+       ('Job search'),
+       ('Remote work'),
+       ('Career advice'),
+       ('Salary'),
+       ('Tech skills'),
+       ('Management');
