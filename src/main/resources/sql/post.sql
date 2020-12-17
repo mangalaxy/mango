afc1a8a37ff7000cb8ba668576e8ad7faf50e9d8
Yuri Podolsky <podolsky4@gmail.com>
2020-12-17 16:38:49 +0200

    Add sql files to fill up
    Signed-off-by: Yuri Podolsky <podolsky4@gmail.com>

---
+++ src/main/resources/sql/post.sql
@@ -0,0 +1,61 @@
+INSERT INTO public.post (headline, opening, author, image_url, content, views_count, likes_count, topic_id, created_date)
+VALUES ('Effective ways of finding a job in 2021',
+        'Unlike in the past, people tend to change their jobs 10-15 times during their professional careers nowadays. New strategies are appearing almost yearly.',
+        'Misha Misco', 'https://media-exp1.licdn.com/dms/image/C4E12AQG3JR0QD1z1ig/article-cover_image-shrink_720_1280/0/1558102615420?e=1613606400&v=beta&t=e-7v_cpdei1WRSgeMTYbMp0gA1uG_BQ46So69NpVE8c',
+        'Unlike in the past, people tend to change their jobs 10-15 times during their professional careers nowadays. New strategies are appearing almost yearly.
+
+In the field of IT, it’s common after working 3-5 years for one company to make a change and apply for a better-paid position in a superior company. But it can be hard since the candidate gets used to his comfort zone and falls out of new tendencies and demands of the market. The first step to a career move is to find an effective way to apply. Here are some basic channels one can use:
+- Googling
+- Company’s Website
+- Networking
+- Social Media Channels
+- Job Fairs
+
+But for specialized professionals who want to use their full potential 2 main sources are preferable: recruitment agencies and specialized job search platforms. Let’s look into them in more depth and find out which one is more effective.
+
+### Recruitment Agencies
+Employers pay recruitment agencies to find hard or highly-specified candidates in short terms. From an employee''s perspective, it’s free and can be effective. But there are some disadvantages in the way recruiting agencies work:
+
+Recruiting agencies are working manually. They normally have a limited amount of staff say 30 who are looking through top employment-oriented sites e.g. Linkedin and making databases of around 10000 workers each and choose candidates whose tech stack, experience and skills correspond to the opening. This takes a lot of time and is not efficient enough.
+
+### Job Search Platforms
+These are specialist sites that specialize in a particular profession, geographic region, or even a specific city or community. Job search platforms work faster. They use machine learning algorithms to rely on unlike manual labor recruiting agencies. The pros for a candidate are:
+Faster overall hiring process. Specialized job search platform will push companies to speed up with interview process
+Percentage of hiring is high since only companies who are in search of niche roles register
+More guarantees with feedback on interviews
+Companies are attentively looking at candidate’s soft skills, experience, tech stack to find a perfect match
+Professional advice from career advocates
+
+As a team who had experience working for recruitment agencies, we have decided to create a specialist job search platform for IT talents to accelerate processes and use better instruments thus improving the situation on the IT market.
+We wish you good luck on the ManGoStart platform!
+', 0, 0, 3, now());
+
+INSERT INTO public.post (headline, opening, author, image_url, content, views_count, likes_count, topic_id, created_date)
+VALUES ('What do companies expect from potential employees?', 'Though expectations from employees may differ from one company to another, one constant is always true - employers seek employees who are trustworthy, reliable and know their jobs.',
+        'Misha Misco', 'https://www.recruitbpm.com/wp-content/uploads/2017/05/3145c06.jpg',
+        'Though expectations from employees may differ from one company to another, one constant is always true - employers seek employees who are trustworthy, reliable and know their jobs.
+
+We have identified the five most valuable characteristics you should possess to become a wanted employee:
+
+1. Demonstration of dependability:
+To show dependability one needs to take personal ownership of all aspects of their job:
+- being professional and skillful in your field
+- demonstrating a high level of commitment
+- being on time
+- dressing and working in a professional manner
+- demonstrating a high level of commitment
+
+2. Demonstrating self-motivation
+While a manager’s role is to motivate employees, they look for candidates who have inner-drive to organize the work. Being able to work without encouragement and taking the extra mile such as doing some additional studying or research is highly appreciated.
+
+3. Representing the company/brand positively
+Employers want individuals who have nice reputations both inside and outside of work. Besides having a good work ethic, it''s advised to have a positive and trustworthy image on your social media.
+
+4. Being a team player
+It’s impossible to achieve greatness at work completely alone. You need to be an accomplished team player who can highlight your successes and also praise others for theirs. Understanding your professional advantages as well as realizing that others have other advantages and using all skills for the best of the project is the key.
+
+5. Having a positive attitude
+Difficult situations shouldn’t influence your positive attitude. Excellent employees tackle projects and overcome difficulties staying calm. We all know that work can be stressful, with strict deadlines, uncertainty, and conflicts. But there are no issues at work that can’t be solved. Maintaining a positive mood and “can do” feeling is something one should work on.
+
+Let’s sum up. Dear talent, if you feel like you possess all those 5 characteristics, congrats: doors of all well-known companies are open for you. Anyways, stay calm and work on things you are lacking. You will still be chased by companies since there is a hype for IT talents. How long this hype lasts we don’t know, time will show. But you should put enough effort and be ready for changes. It’s never too late to study, right?
+', 0, 0, 5, now());
